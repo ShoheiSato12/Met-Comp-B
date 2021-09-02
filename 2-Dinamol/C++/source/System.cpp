477f900a8b7abd100201e80fd4f6d5a6455271f7
Shohei Sato <shoheisato12@gmail.com>
2021-09-02 11:04:20 -0300

    Class atribution

--- 2-Dinamol/C++/source/System.cpp
+++ 2-Dinamol/C++/source/System.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include<cmath>
 #include<thread>
 #include"../include/System.hpp"
--- 2-Dinamol/C++/source/main.cpp
+++ 2-Dinamol/C++/source/main.cpp
@@ -13,8 +13,8 @@ int main()
     auto start = std::chrono::high_resolution_clock::now();
     
     double size[2]={2, 2};
-    System sys (5,52,0.01,1,size);
-    sys.setL(3);
+    System sys = System(5,52,0.01,1,size);
+    //sys.setL(3.0);
     //std::cout << sys.getX()[5];
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
